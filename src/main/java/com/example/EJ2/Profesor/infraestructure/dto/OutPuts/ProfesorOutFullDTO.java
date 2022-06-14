b35de35869d03411de495df36ab40e24d88498fb
Jorge Balladares <jorge.balladares@bosonit.com>
2022-06-14 13:49:48 +0200

    commitFeign

---
+++ src/main/java/com/example/EJ2/Profesor/infraestructure/dto/OutPuts/ProfesorOutFullDTO.java
@@ -0,0 +1,19 @@
+package com.example.EJ2.Profesor.infraestructure.dto.OutPuts;
+
+import com.example.EJ2.Persona.Infraestructure.dto.Inputs.PersonaInputDTO;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfesorOutFullDTO {
+
+    private String id_profesor;
+    private PersonaInputDTO persona;
+    private String coments;
+    private String branch;
+
+
+    public ProfesorOutFullDTO(String id, String s) {
+    }
+}
